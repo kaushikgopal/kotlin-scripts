5c8c96281630c3bf8a2d56b022522361891658c4
Kaushik Gopal <code@kaush.co>
2017-05-22 15:31:49 -0700

    chore: list folders script

---
+++ list_folders.kts
@@ -0,0 +1,20 @@
+import java.io.File
+
+fun program(args : Array<String>) {
+
+  if (args.isEmpty()) {
+    return
+  }
+
+  val folders = File(args[0]).listFiles { file ->
+    file.isDirectory()
+  }
+
+  folders?.forEach { folder -> println(folder) }
+}
+
+
+// run this program like so `kotlinc -script list_folders.kts ~/code`
+println("******* PROGRAM START ***************** ")
+program(args)
+println("******* PROGRAM END ***************** ")
