e5e0d39ace04d99dd53eab81b3b95d896b9d7e62
Kaushik Gopal <1042037+kaushikgopal@users.noreply.github.com>
2023-06-01 16:46:48 -0700

    feat|new example embedding a kts file in another

---
+++ 8.embed-external-scripts.main.kts
@@ -0,0 +1,29 @@
+#!/usr/bin/env kotlin
+
+@file:Import("./myClassObject.kts")
+
+import java.io.File
+
+/*
+ * This script demonstrates how you can import or embed external kts scripts
+ * run this script like so:
+
+    $>  brew install kotlin
+    $>  kotlin 8.embed-external-scripts.main.kts
+ *
+ * alternatively if you want to make the script executable
+
+    $>  chmod +x 8.embed-external-scripts.main.kts
+    $>  ./8.embed-external-scripts.main.kts
+*/
+
+println("******* PROGRAM START ***************** ")
+program(args)
+println("******* PROGRAM END ***************** ")
+
+
+
+fun program(args : Array<String>) {
+    val person = Person(name = "Albert Einstein", age = 76)
+    println("printing from class MyClassObject - ${person.name} is ${person.age} years old")
+}
---
+++ myClassObject.kts
@@ -0,0 +1,6 @@
+#!/usr/bin/env kotlin
+
+data class Person(
+  val name: String,
+  val age: Int
+)
